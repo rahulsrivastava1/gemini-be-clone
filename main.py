531d32102be27f443a9fe830cc800886e59f0dd4
Rahul Srivastava <rahul.srivastava@nathabit.in>
2025-07-17 12:27:13 +0530

    port binding

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 
 from auth.router import router, user_router
@@ -8,3 +10,9 @@ app = FastAPI()
 app.include_router(router)
 app.include_router(user_router)
 app.include_router(chatroom_router)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    port = int(os.environ.get("PORT", 4000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
